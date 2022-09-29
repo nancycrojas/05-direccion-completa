d8340ef88a96f8a793bd1e08d220bd437af0f733
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 16:10:01 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 //Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa, p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina".
 
 
-const calle = prompt ("Por favor ingresa tu calle");
-const numero = prompt ("Por favor ingresa tu número");
-const departamento = prompt ("Por favor ingresa tu departamento");
-const codigoPostal = prompt ("Por favor ingresa tu código postal");
-const ciudad = prompt ("Por favor ingresa tu ciudad");
-const pais = prompt ("Por favor ingresa tu país");
-alert("La dirección que ha ingresado es: " + calle + " " + numero + ", " + departamento + ", " + codigoPostal + ", " + ciudad + ", " + pais + ".");
+const calle = prompt("Por favor ingresa tu calle");
+const numero = prompt("Por favor ingresa tu número");
+const departamento = prompt("Por favor ingresa tu departamento");
+const codigoPostal = prompt("Por favor ingresa tu código postal");
+const ciudad = prompt("Por favor ingresa tu ciudad");
+const pais = prompt("Por favor ingresa tu país");
+//alert("La dirección que ha ingresado es: " + calle + " " + numero + ", " + departamento + ", " + codigoPostal + ", " + ciudad + ", " + pais + ".");
+
+//Template strings
+alert(`La dirección que ha ingresado es: ${calle} ${numero}, ${departamento}, ${codigoPostal}, ${ciudad}, ${pais}.`)
